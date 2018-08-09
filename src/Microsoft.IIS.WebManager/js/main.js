1365571aabc7bb9fb18c96ade28f6df6bbf14f9d
Peter Hsu <shhsu@microsoft.com>
2018-08-09 10:30:11 -0700

    * Change the way we do versioning * Copy all css files when building (This fix is not directly related to WAC feature)

--- src/Microsoft.IIS.WebManager/js/main.js
+++ src/Microsoft.IIS.WebManager/js/main.js
@@ -11,7 +11,7 @@
         }, 1)
     });
 
-    $.get('./package.v' + SETTINGS.version + '.json', function (appData) {
+    $.get('./package.json?v=' + SETTINGS.version, function (appData) {
 
         function ver(name) {
 
@@ -22,43 +22,43 @@
             return '0.0.0';
         }
 
-        System.config({
+        SystemJS.config({
             defaultJSExtensions: true,
             map: {
 
                 // angular bundles
-                '@angular/core': 'node_modules/@angular/core/bundles/core.umd.v' + ver('@angular/core') + '.js',
-                '@angular/common': 'node_modules/@angular/common/bundles/common.umd.v' + ver('@angular/common') + '.js',
-                '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.v' + ver('@angular/compiler') + '.js',
-                '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.v' + ver('@angular/platform-browser') + '.js',
-                '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.v' + ver('@angular/platform-browser-dynamic') + '.js',
-                '@angular/http': 'node_modules/@angular/http/bundles/http.umd.v' + ver('@angular/http') + '.js',
-                '@angular/router': 'node_modules/@angular/router/bundles/router.umd.v' + ver('@angular/router') + '.js',
-                '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.v' + ver('@angular/forms') + '.js',
-                '@angular/upgrade': 'node_modules/@angular/upgrade/bundles/upgrade.umd.v' + ver('@angular/upgrade') + '.js',
+                '@angular/core': 'node_modules/@angular/core/bundles/core.umd.js?v=' + ver('@angular/core'),
+                '@angular/common': 'node_modules/@angular/common/bundles/common.umd.js?v=' + ver('@angular/common'),
+                '@angular/compiler': 'node_modules/@angular/compiler/bundles/compiler.umd.js?v=' + ver('@angular/compiler'),
+                '@angular/platform-browser': 'node_modules/@angular/platform-browser/bundles/platform-browser.umd.js?v=' + ver('@angular/platform-browser'),
+                '@angular/platform-browser-dynamic': 'node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js?v=' + ver('@angular/platform-browser-dynamic'),
+                '@angular/http': 'node_modules/@angular/http/bundles/http.umd.js?v=' + ver('@angular/http'),
+                '@angular/router': 'node_modules/@angular/router/bundles/router.umd.js?v=' + ver('@angular/router'),
+                '@angular/forms': 'node_modules/@angular/forms/bundles/forms.umd.js?v=' + ver('@angular/forms'),
+                '@angular/upgrade': 'node_modules/@angular/upgrade/bundles/upgrade.umd.js?v=' + ver('@angular/upgrade'),
 
 
-                'chart.js': 'node_modules/chart.js/dist/chart.min.v' + ver('@angular/upgrade') + '.js',
-                'ng2-charts': 'node_modules/ng2-charts/charts/charts.v' + ver('@angular/upgrade') + '.js',
+                'chart.js': 'node_modules/chart.js/dist/chart.min.js?v=' + ver('@angular/upgrade'),
+                'ng2-charts': 'node_modules/ng2-charts/charts/charts.js?v=' + ver('@angular/upgrade'),
 
                 // other libraries
                 'angulartics2': 'node_modules/angulartics2',
                 'rxjs': 'node_modules/rxjs',
-                'ts': 'node_modules/plugin-typescript@4.0.10/lib/plugin.v' + ver('ts') + '.js',
-                'typescript': 'node_modules/typescript@2.0.2/lib/typescript.v' + ver('typescript') + '.js'
+                'ts': 'node_modules/plugin-typescript@4.0.10/lib/plugin.js?v=' + ver('ts'),
+                'typescript': 'node_modules/typescript@2.0.2/lib/typescript.js?v=' + ver('typescript')
             },
             packages: {
                 defaultExtension: 'js',
                 app: {
                     format: 'cjs',
-                    defaultExtension: 'v' + SETTINGS.version + '.js'
+                    defaultExtension: 'js?v=' + SETTINGS.version
                 },
                 angulartics2: {
                     main: 'index',
-                    defaultExtension: 'v' + ver('angulartics2') + '.js'
+                    defaultExtension: 'js?v=' + ver('angulartics2')
                 },
                 rxjs: {
-                    defaultExtension: 'v' + ver('rxjs') + '.js'
+                    defaultExtension: 'js?v=' + ver('rxjs')
                 }
             }
         });
